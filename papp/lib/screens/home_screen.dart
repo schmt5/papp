ad4ce3f5e33a873b6af66f5c1f684a3b46885860
SoKeY <sch-midi@windowslive.com>
2019-10-31 11:40:37 +0100

    individual TabController in HomeScreen
    Is needed, because dashboardScreen doesnt need a FAB, but AppointmentOverviewScreen does

--- papp/lib/screens/home_screen.dart
+++ papp/lib/screens/home_screen.dart
@@ -5,32 +5,70 @@ import './appointment_overview_screen.dart';
 
 import '../widgets/app_drawer.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen>
+    with SingleTickerProviderStateMixin {
+  TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(vsync: this, length: 2);
+    _tabController.addListener(_handleTabIndex);
+  }
+
+  @override
+  void dispose() {
+    _tabController.removeListener(_handleTabIndex);
+    _tabController.dispose();
+    super.dispose();
+  }
+
+  void _handleTabIndex() {
+    setState(() {});
+  }
+
+  Widget _buildFab() {
+    return _tabController.index == 0
+        ? null
+        : FloatingActionButton.extended(
+            label: Text('Neuer Termin erfassen'),
+            icon: Icon(Icons.add),
+            onPressed: () {},
+          );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      child: Scaffold(
-        drawer: AppDrawer(),
-        appBar: AppBar(
-          title: Text('Home'),
-          bottom: TabBar(
-            tabs: <Widget>[
-              Tab(
-                text: 'Dashboard',
-              ),
-              Tab(
-                text: 'Termine',
-              )
-            ],
-          ),
-        ),
-        body: TabBarView(
-          children: <Widget>[
-            DashboardScreen(),
-            AppointmentOverviewScreen(),
+    return Scaffold(
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: _buildFab(),
+      drawer: AppDrawer(),
+      appBar: AppBar(
+        title: Text('Home'),
+        bottom: TabBar(
+          controller: _tabController,
+          tabs: <Widget>[
+            Tab(
+              text: 'Dashboard',
+            ),
+            Tab(
+              text: 'Termine',
+            )
           ],
+          indicatorColor: Colors.white,
         ),
+      ),
+      body: TabBarView(
+        controller: _tabController,
+        children: <Widget>[
+          DashboardScreen(),
+          AppointmentOverviewScreen(),
+        ],
       ),
     );
   }
