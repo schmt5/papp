f94eb2a4294aca399beab318a29c06dd97511fea
SoKeY <sch-midi@windowslive.com>
2019-12-09 14:26:33 +0100

    AppointmentOverviewScreen: add Kein Termin Icon
    Zeigt Kein Termin an, falls es keinen Termin hat.

--- papp/lib/screens/appointment_overview_screen.dart
+++ papp/lib/screens/appointment_overview_screen.dart
@@ -12,38 +12,60 @@ class AppointmentOverviewScreen extends StatelessWidget {
           .fetchAndSetAppointments(),
       builder: (ctx, snapshot) {
         return Consumer<Appointments>(
-          builder: (ctx, data, child) => ListView.separated(
-            itemCount: data.upcomingItems.length,
-            itemBuilder: (ctx, i) => AppointmentItem(data.upcomingItems[i]),
-            separatorBuilder: (ctx, i) {
-              var j = i == (data.upcomingItems.length - 1) ? i : i + 1;
-              return Padding(
-                padding: const EdgeInsets.symmetric(vertical: 8),
-                child:
-                    (data.upcomingItems[i].dateTime.day == DateTime.now().day &&
-                            data.upcomingItems[j].dateTime.day !=
-                                DateTime.now().day)
-                        ? Container(
-                            width: double.infinity,
-                            height: 30,
-                            decoration: BoxDecoration(
-                              color: Colors.black54,
-                              borderRadius: BorderRadius.circular(3),
-                            ),
-                            child: Center(
-                              child: Text(
-                                'Nicht mehr heute',
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.bold,
+          builder: (ctx, data, child) => data.upcomingItems.length == 0
+              ? Center(
+                  child: Column(
+                    children: <Widget>[
+                      SizedBox(
+                        height: 120,
+                      ),
+                      Icon(
+                        Icons.event_busy,
+                        size: 72,
+                      ),
+                      Text(
+                        'Keine Termine',
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+              : ListView.separated(
+                  itemCount: data.upcomingItems.length,
+                  itemBuilder: (ctx, i) =>
+                      AppointmentItem(data.upcomingItems[i]),
+                  separatorBuilder: (ctx, i) {
+                    var j = i == (data.upcomingItems.length - 1) ? i : i + 1;
+                    return Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 8),
+                      child: (data.upcomingItems[i].dateTime.day ==
+                                  DateTime.now().day &&
+                              data.upcomingItems[j].dateTime.day !=
+                                  DateTime.now().day)
+                          ? Container(
+                              width: double.infinity,
+                              height: 30,
+                              decoration: BoxDecoration(
+                                color: Colors.black54,
+                                borderRadius: BorderRadius.circular(3),
+                              ),
+                              child: Center(
+                                child: Text(
+                                  'Nicht mehr heute',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                  ),
                                 ),
                               ),
-                            ),
-                          )
-                        : Container(),
-              );
-            },
-          ),
+                            )
+                          : Container(),
+                    );
+                  },
+                ),
         );
       },
     );
